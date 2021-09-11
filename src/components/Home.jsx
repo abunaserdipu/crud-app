d97c13a7465b112485f42062850a88bb5d2a2156
abunaserdipu <abunaserdipu0@gmail.com>
2021-09-12 03:02:11 +0600

    added banner & functionalities

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Navbar from "./Navbar";
 
 function Home() {
   return (
-    <div>
+    <>
       <Navbar />
-      <h1>This is homepage</h1>
-    </div>
+      <div className="banner">
+        <div className="content">
+          <h2>Web services provider</h2>
+          <p>We develop innovative and creative products and services that provide total communication and information solutions</p>
+          <Link to="/users" className="btn">Our Work</Link>
+        </div>
+      </div>
+    </>
   );
 }
 
